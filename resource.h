c4b3a1508579afba06c589093224a4094cb0af7f
Dedmen Miller <dedmen@dedmen.de>
2023-09-02 14:19:04 +0200

    Add PBO Icon from @BaerMitUmlaut

--- resource.h
+++ resource.h
@@ -3,6 +3,7 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
+#define IDI_ICON1                       105
 #define IDD_PROPPAGE_LARGE              107
 #define IDC_LIST1                       1004
 #define IDC_EDIT1                       1005
@@ -11,7 +12,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
